c1c095fe3168a24669a2bf50dff72d749bc63c5f
Igor <ikor1321@yandex.ru>
2024-05-13 23:06:19 +0300

    <style>:<удалил лишние комментарии>

--- stats-service/stats-client/src/main/java/ru/practicum/StatServiceClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/StatServiceClient.java
@@ -15,10 +15,10 @@ import java.util.Map;
 @Service
 public class StatServiceClient extends BaseClient {
     @Autowired
-    public StatServiceClient(@Value("${statsService.url}") String url,  RestTemplateBuilder templateBuilder) {
+    public StatServiceClient(RestTemplateBuilder templateBuilder) {
         super(
                 templateBuilder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(/*url */"http://stats-server:9090"))
+                        .uriTemplateHandler(new DefaultUriBuilderFactory("http://stats-server:9090"))
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
